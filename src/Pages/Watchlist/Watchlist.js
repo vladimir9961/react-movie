3fa516af2a07d89e0f7cf5a20199a18f993c92b6
vladimir9961 <wlada725@gmail.com>
2022-10-26 22:17:44 +0200

    nav fix

--- src/Pages/Watchlist/Watchlist.js
+++ src/Pages/Watchlist/Watchlist.js
@@ -1,14 +1,17 @@
 import { useEffect, useState } from 'react'
-import { Card, Col, Container, Row, Badge, Form } from 'react-bootstrap';
+import { Card, Col, Container, Row, Badge, Form, Button } from 'react-bootstrap';
 import FavoriteBtn from '../../Components/Cards/userActions/FavoriteBtn'
 import WatchlistBtn from '../../Components/Cards/userActions/WatchlistBtn'
+import Rate from '../../Components/Cards/userActions/Rate'
 let session = localStorage.getItem('session-id');
 let accId = localStorage.getItem('user-id');
 export default function Watchlist() {
     const [watchlist, setwatchlist] = useState(null)
     const [type, setType] = useState("movie")
+    const [rating, setRating] = useState(0);
     const [existFavorite, setExistFavorite] = useState(false);
     const [existWatchlist, setExistWatchlist] = useState(false);
+    const [showRate, setShowRate] = useState(false)
     const styles = {
         favorite: {
             color: "black"
@@ -19,16 +22,34 @@ export default function Watchlist() {
         // list: {
         //     display: showlist ? "block" : "none"
         // },
-        // rate: {
-        //     display: showRate ? "flex" : "none"
-        // }
+        rate: {
+            display: showRate ? "flex" : "none",
+        }
     }
     function removeItem(id) {
         const newWatchlist = watchlist.filter((l) => l.id !== id)
         setwatchlist(newWatchlist)
     }
 
-
+    const rated = (id) => {
+        fetch(`
+       https://api.themoviedb.org/3/account/${accId}/rated/${type === "movie" ? "movies" : "tv"}?api_key=3b5caee89d6f1ccfb03cb837adb8e9e1&language=en-US&session_id=${session}&sort_by=created_at.asc&page=1`)
+            .then(res => res.json())
+            .then((data) => {
+                let obj = data.results.find(o => o.id === id);
+                if (obj != undefined) {
+                    setRating(obj.rating);
+                }
+            })
+    }
+    //DISPLAY CONTAINER STARS
+    const displayContainerWithStars = (e, id) => {
+        if (e.currentTarget === e.target) {
+            console.log(e.currentTarget)
+            rated(id)
+            setShowRate((prevsetShowList) => !showRate)
+        }
+    }
     useEffect(() => {
         const fetchData = async () => {
             const data = await fetch(`https://api.themoviedb.org/3/account/${accId}/watchlist/${type === "movie" ? "movies" : "tv"}?api_key=3b5caee89d6f1ccfb03cb837adb8e9e1&language=en-US&session_id=${session}&sort_by=created_at.desc&page=1`);
@@ -46,9 +67,7 @@ export default function Watchlist() {
                 onClick={(e) => {
                     if (e.target.checked === true) {
                         setType("tv")
-                        console.log(type)
                     } else {
-                        console.log(type)
                         setType("movie")
                     }
                 }}
@@ -94,6 +113,28 @@ export default function Watchlist() {
                                                 </Card.Text>
                                             </Col>
                                             <Col className='col-12 d-flex'>
+                                                <Button
+                                                    eventKey="4"
+                                                    key-id={result.id}
+                                                    key-type={type}
+                                                    id="rate"
+                                                    onClick={(e) =>
+                                                        displayContainerWithStars(e, result.id)
+                                                    }
+                                                >   Rate
+                                                    <Container
+                                                        fluid
+                                                        style={styles.rate}
+                                                        className="rate-container"
+                                                    >
+                                                        <Rate
+                                                            id={result.id}
+                                                            value={rating}
+                                                            type={type}
+                                                            aria-labelledby="add rate"
+                                                        />
+                                                    </Container>
+                                                </Button>
                                                 <FavoriteBtn
                                                     aria-labelledby="add to favorite"
                                                     style={styles.favorite}
